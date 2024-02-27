8610e12319de6e1e769d199e6735881905e140cf
Khalid Khandaker <khalidkhandaker16@gmail.com>
2024-02-27 18:08:09 +0800

    splash screen complete

---
+++ app/src/main/java/com/example/cherry/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.cherry;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.splashscreen.SplashScreen;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen splashScreen = SplashScreen.installSplashScreen(this);
+
+        View content = findViewById(android.R.id.content);
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
